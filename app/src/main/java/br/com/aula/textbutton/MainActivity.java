c5eef130feede85a9495e1de003d484f6c63076f
Lucas Martins <lucasmartins1096@hotmail.com>
2024-08-17 22:21:32 -0300

    Meu projeto

---
+++ app/src/main/java/br/com/aula/textbutton/MainActivity.java
@@ -0,0 +1,38 @@
+package br.com.aula.textbutton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+
+    public void alterarTexto(View view){
+        TextView resultado = findViewById(R.id.textoResultado);
+        resultado.setText("Você clicou no botão");
+    }
+
+    public void limparTexto(View view){
+        TextView resultado = findViewById(R.id.textoResultado);
+        resultado.setText("");
+    }
+}
